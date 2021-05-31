83b204de2c5c303459efc4747e2a8255ac8c02a5
이동걸 <oww@mz.co.kr>
2021-05-31 09:25:50 +0900

    update!

--- wwwroot/admin/html/include_header.js
+++ wwwroot/admin/html/include_header.js
@@ -2,91 +2,106 @@ $(function(){
 
 
     var header = `
-        <header class="header">
-			<div class="header-top">
-				header
+	<div class="sidebar-menu">
+	<div class="sidebar-menu-inner">
+		<header class="logo-env">
+			<!-- logo collapse icon -->
+			<div class="sidebar-collapse">
+				<a href="#" class="sidebar-collapse-icon">
+				<!-- add class "with-animation" if you want sidebar to have animation during expanding/collapsing transition --><i class="entypo-menu"></i> </a>
 			</div>
-			<div class="gnb-wrap">
-				<ul class="gnb-area">
-					<li>
-						<strong class="lnb-toggle-bt" data-target="lnb-area-01" data-on="true" data-siblings="true">판매상품 관리</strong>
-						<ul class="lnb-area lnb-area-01">
-							<li><a href="">일반상품 목록</a></li>
-							<li><a href="">사은품 목록</a></li>
-							<li><a href="">재입고 알림</a></li>
-							<li><a href="">카테고리 관리</a></li>
-							<li><a href="">브랜드 관리</a></li>
-							<li><a href="">상품 전시 관리</a></li>
-						</ul>
-					</li>
-					<li>
-						<strong class="lnb-toggle-bt" data-target="lnb-area-02" data-on="true" data-siblings="true">주문 관리</strong>
-						<ul class="lnb-area lnb-area-02">
-							<li><a href="">통합 주문 목록 <span>11</span></a></li>
-							<li><a href="">입금대기 목록 <span>11</span></a></li>
-							<li><a href="">결제완료 목록 <span>11</span></a></li>
-							<li><a href="">상품준비중 목록 <span>11</span></a></li>
-							<li><a href="">송장일괄등록</a></li>
-							<li><a href="">배송중 목록 <span>11</span></a></li>
-							<li><a href="">배송완료 목록 <span>11</span></a></li>
-							<li><a href="">구매확정 목록 <span>11</span></a></li>
-							<li><a href="">결제 중단/실패 목록 <span>11</span></a></li>
-							<li><a href="">현금영수증 발급/조회</a></li>
-							<li><a href="">수동 주문 등록</a></li>
-						</ul>
-					</li>
-					<li>
-						<strong class="lnb-toggle-bt" data-target="lnb-area-03" data-on="true" data-siblings="true">취소/반품/교환 관리</strong>
-						<ul class="lnb-area lnb-area-03">
-							<li><a href="">취소 목록 <span>11</span></a></li>
-							<li><a href="">교환 목록 <span>11</span></a></li>
-							<li><a href="">반품 목록 <span>11</span></a></li>
-							<li><a href="">환불 목록 <span>11</span></a></li>
-						</ul>
-					</li>
-					<li>
-						<strong class="lnb-toggle-bt" data-target="lnb-area-04" data-on="true" data-siblings="true">회원 관리</strong>
-						<ul class="lnb-area lnb-area-04">
-							<li><a href="">회원 목록</a></li>
-							<li><a href="">휴면 회원 목록</a></li>
-							<li><a href="">탈퇴 회원 목록</a></li>
-							<li><a href="">카카오 알림톡</a></li>
-							<li><a href="">SMS 발송 관리</a></li>
-							<li><a href="">SMS 대량발송</a></li>
-							<li><a href="">이메일 발송 관리</a></li>
-							<li><a href="">이메일 대량 발송</a></li>
-							<li><a href="">리마인드 메시지 관리</a></li>
-						</ul>
-					</li>
-					<li>
-						<strong class="lnb-toggle-bt" data-target="lnb-area-05" data-on="true" data-siblings="true">게시판 관리</strong>
-						<ul class="lnb-area lnb-area-05">
-							<li><a href="">게시판 목록</a></li>
-							<li><a href="">게시글 관리</a></li>
-							<li><a href="">금칙어 관리</a></li>
-							<li><a href="">FAQ 관리</a></li>
-						</ul>
-					</li>
-					<li>
-						<a href="">프로모션 관리</a>
-					</li>
-					<li>
-						<a href="">마케팅 관리</a>
-					</li>
-					<li>
-						<a href="">통계</a>
-					</li>
-					
-				</ul>
-				<button type="button" class="gnb-toggle-bt" data-target="gnb-wrap" data-on="true" data-sort="none"></button>
+			<!-- logo -->
+			<div class="logo">
+				<a href=""> 
+					NATURE REPUBLIC
+				</a>
 			</div>
+			<!-- open/close menu icon (do not remove if you want to enable menu on mobile devices) -->
+			<div class="sidebar-mobile-menu visible-xs">
+				<a href="#" class="with-animation">
+				<!-- add class "with-animation" to support animation --><i class="entypo-menu"></i> </a>
+			</div>
+			
 		</header>
+		<ul id="main-menu" class="main-menu">
+			<li>
+				<a href="" title="판매상품 관리" data-menunum="1"><i class="ico head01"></i><span>판매상품 관리</span></a>
+				<ul class="has-sub">
+					<li><a href="" title="" data-menunum="1"><span>일반상품 목록</span></a></li>
+					<li><a href="" title="" data-menunum="2"><span>사은품 목록</span></a></li>
+					<li><a href="" title="" data-menunum="3"><span>재입고 알림</span></a></li>
+					<li><a href="" title="" data-menunum="4"><span>카테고리 관리</span></a></li>
+					<li><a href="" title="" data-menunum="5"><span>브랜드 관리</span></a></li>
+					<li><a href="" title="" data-menunum="6"><span>상품 전시 관리</span></a></li>
+				</ul>
+			</li>
+			<li>
+				<a href="" title="주문 관리" data-menunum="2"><i class="ico head02"></i><span>주문 관리</span></a>
+				<ul class="has-sub">
+					<li><a href="" title="" data-menunum="1"><span>통합 주문 목록 <em>11</em></span></a></li>
+					<li><a href="" title="" data-menunum="2"><span>입금대기 목록 <em>11</em></span></a></li>
+					<li><a href="" title="" data-menunum="3"><span>결제완료 목록 <em>11</em></span></a></li>
+					<li><a href="" title="" data-menunum="4"><span>상품준비중 목록 <em>11</em></span></a></li>
+					<li><a href="" title="" data-menunum="5"><span>송장일괄등록 <em>11</em></span></a></li>
+					<li><a href="" title="" data-menunum="6"><span>배송중 목록 <em>11</em></span></a></li>
+					<li><a href="" title="" data-menunum="7"><span>배송완료 목록 <em>11</em></span></a></li>
+					<li><a href="" title="" data-menunum="8"><span>구매확정 목록 <em>11</em></span></a></li>
+					<li><a href="" title="" data-menunum="9"><span>결제 중단/실패 목록 <em>11</em></span></a></li>
+					<li><a href="" title="" data-menunum="10"><span>현금영수증 발급/조회 <em>11</em></span></a></li>
+					<li><a href="" title="" data-menunum="11"><span>수동 주문 등록 <em>11</em></span></a></li>
+				</ul>
+			</li>
+			<li>
+				<a href="" title="취소/반품/교환 관리" data-menunum="3"><i class="ico head03"></i><span>취소/반품/교환 관리</span></a>
+				<ul class="has-sub">
+					<li><a href="" title="" data-menunum="1"><span>취소 목록 <em>11</em></span></a></li>
+					<li><a href="" title="" data-menunum="2"><span>교환 목록 <em>11</em></span></a></li>
+					<li><a href="" title="" data-menunum="3"><span>반품 목록 <em>11</em></span></a></li>
+					<li><a href="" title="" data-menunum="4"><span>환불 목록 <em>11</em></span></a></li>
+				</ul>
+			</li>
+			<li>
+				<a href="" title="회원 관리" data-menunum="4"><i class="ico head04"></i><span>회원 관리</span></a>
+				<ul class="has-sub">
+					<li><a href="" title="" data-menunum="1"><span>회원 목록</span></a></li>
+					<li><a href="" title="" data-menunum="2"><span>휴면 회원 목록</span></a></li>
+					<li><a href="" title="" data-menunum="3"><span>탈퇴 회원 목록</span></a></li>
+					<li><a href="" title="" data-menunum="4"><span>카카오 알림톡</span></a></li>
+					<li><a href="" title="" data-menunum="5"><span>SMS 발송 관리</span></a></li>
+					<li><a href="" title="" data-menunum="6"><span>SMS 대량발송</span></a></li>
+					<li><a href="" title="" data-menunum="7"><span>이메일 발송 관리</span></a></li>
+					<li><a href="" title="" data-menunum="8"><span>이메일 대량 발송</span></a></li>
+					<li><a href="" title="" data-menunum="9"><span>리마인드 메시지 관리</span></a></li>
+				</ul>
+			</li>
+			<li>
+				<a href="" title="게시판 관리" data-menunum="5"><i class="ico head05"></i><span>게시판 관리</span></a>
+				<ul class="has-sub">
+					<li><a href="" title="" data-menunum="1"><span>게시판 목록</span></a></li>
+					<li><a href="" title="" data-menunum="2"><span>게시글 관리</span></a></li>
+					<li><a href="" title="" data-menunum="3"><span>금칙어 관리</span></a></li>
+					<li><a href="" title="" data-menunum="4"><span>FAQ 관리</span></a></li>
+				</ul>
+			</li>
+			<li>
+				<a href="" title="" data-menunum="6"><i class="ico head06"></i><span>프로모션 관리</span></a>
+			</li>
+			<li>
+				<a href="" title="" data-menunum="7"><i class="ico head07"></i><span>마케팅 관리</span></a>
+			</li>
+			<li>
+				<a href="" title="" data-menunum="8"><i class="ico head08"></i><span>통계</span></a>
+			</li>
+
+			
+		</ul>
+	</div>
+</div>
     `;
 
     
-	console.log(111);
-    $('.header').html(header);
-    $('.header .header').unwrap();
+    $('.sidebar-menu').html(header);
+    $('.sidebar-menu .sidebar-menu').unwrap();
 
 
 });
